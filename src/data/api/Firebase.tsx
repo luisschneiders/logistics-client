3d4d117a0140e6d910cd0c273af949f6fe6444d6
Luis Schneiders <schneiders.tech@gmail.com>
2021-04-21 17:23:28 +1000

    create User and Company in Firestore when RegisterUser function is called; create function addFirestoreCollectionUser and addFirestoreCollectionCompany

--- src/data/api/Firebase.tsx
+++ src/data/api/Firebase.tsx
@@ -5,9 +5,12 @@ import { toast } from '../../components/toast/Toast';
 import { Firebase } from '../../credentials/Firebase';
 import { StatusColor } from '../../enum/StatusColor';
 import { UserProfileFirebase } from '../../models/UserProfileFirebase';
+import { CollectionUser } from '../../models/CollectionUser';
+import { CollectionCompany } from '../../models/CollectionCompany';
+import { RegisterCompanyForm } from '../../models/RegisterCompanyForm';
 
 firebase.initializeApp(Firebase);
-// const db = firebase.firestore();
+const db = firebase.firestore();
 
 export async function loginUser(email: string, password: string) {
   try {
@@ -20,11 +23,33 @@ export async function loginUser(email: string, password: string) {
   }
 }
 
-export async function registerUser(email: string, password: string) {
+export async function registerUser(form: RegisterCompanyForm) {
   try {
-    const response: any = await firebase.auth().createUserWithEmailAndPassword(email, password);
-    toast('Successfully registered!', StatusColor.DEFAULT);
-    return response;
+    const resultRegisterUser: any = await firebase.auth().createUserWithEmailAndPassword(form.email, form.password);
+
+    const addCollectionCompany: CollectionCompany = {
+      companyId: resultRegisterUser.user.uid,
+      companyName: form.companyName,
+      companyAbnAcn: form.companyAbnAcn,
+      companySignup: true,
+      companyType: form.companyType,
+      companyCreatedBy: resultRegisterUser.user.uid,
+      companyEmail: form.email,
+    }
+
+    const addCollectionUser: CollectionUser = {
+      companyId: resultRegisterUser.user.uid,
+      userId: resultRegisterUser.user.uid,
+      userEmail: form.email,
+      userName: form.userName,
+      userRole: 'admin',
+    }
+
+    const resultCollectionCompany: any = await db.collection('Company').add(addCollectionCompany);
+    const resultCollectionUser: any = await db.collection('User').add(addCollectionUser);
+
+    return {resultRegisterUser, resultCollectionCompany, resultCollectionUser};
+
   } catch(error) {
     toast(error.message, StatusColor.ERROR, 4000);
     return false;
@@ -58,4 +83,14 @@ export async function updateProfile(profile: UserProfileFirebase) {
     toast(error.message, StatusColor.ERROR, 4000);
     return false;
   }
+}
+
+export async function addFirestoreCollectionUser(collectionUser: CollectionUser) {
+  const response: any = await db.collection('User').add(collectionUser);
+  return response;
+}
+
+export async function addFirestoreCollectionCompany(collectionCompany: CollectionCompany) {
+  const response: any = await db.collection('Company').add(collectionCompany);
+  return response;
 }
