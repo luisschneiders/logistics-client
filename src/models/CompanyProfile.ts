20b8822abcd78463cf4ffe7075b4934e6c73d8cb
Luis Schneiders <schneiders.tech@gmail.com>
2021-04-22 22:02:51 +1000

    update CompanyProfile model

--- src/models/CompanyProfile.ts
+++ src/models/CompanyProfile.ts
@@ -4,7 +4,6 @@ export interface CompanyProfile {
   companyId: string;
   companyName: string;
   companyAbnAcn: string;
-  companySignup: boolean;
   companyType: CompanyType;
   companyCreatedBy: string;
   companyEmail: string;
