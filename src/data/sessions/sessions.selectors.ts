bdb1d715d787e210d0a64bd29be8fc97683d54c8
Luis Schneiders <schneiders.tech@gmail.com>
2021-05-07 21:14:48 +1000

    add new selector getCompanyUser

--- src/data/sessions/sessions.selectors.ts
+++ src/data/sessions/sessions.selectors.ts
@@ -1,9 +1,11 @@
 import { createSelector } from 'reselect';
 import { CompanyProfile } from '../../models/CompanyProfile';
+import { CompanyUser } from '../../models/CompanyUser';
 import { Period } from '../../models/Period';
 import { AppState } from '../app/app.state';
 
 const getCompanyProfileData = (state: AppState) => state.sessionsReducer.companyProfile;
+const getCompanyUserData = (state: AppState) => state.sessionsReducer.companyUser;
 const getHomeTimeTransitionData = (state: AppState) => state.sessionsReducer.homeTimeTransition;
 const getExpensesTimeTransitionData = (state: AppState) => state.sessionsReducer.expensesTimeTransition;
 const getTransactionsTimeTransitionData = (state: AppState) => state.sessionsReducer.transactionsTimeTransition;
@@ -33,5 +35,12 @@ export const getCompanyProfile = createSelector(
   getCompanyProfileData,
   (companyProfile: CompanyProfile) => {
     return companyProfile;
+  }
+);
+
+export const getCompanyUser = createSelector(
+  getCompanyUserData,
+  (companyUser: CompanyUser) => {
+    return companyUser;
   }
 );
