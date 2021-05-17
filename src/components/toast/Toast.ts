2d3a37d5a3080a83fce777f857ba370e0700b2e1
Luis Schneiders <schneiders.tech@gmail.com>
2021-05-17 20:27:36 +1000

    update component UI and behaviour

--- src/components/toast/Toast.ts
+++ src/components/toast/Toast.ts
@@ -1,10 +1,15 @@
 import { StatusColor } from '../../enum/StatusColor';
 
-export const toast = (message: string, color: string = StatusColor.DEFAULT, duration: number = 2000) => {
+export const toast = (
+    message: string,
+    color: string = StatusColor.DEFAULT,
+    duration: number = 2000,
+    position: string = 'top') => {
   const toast: any = document.createElement('ion-toast');
   toast.message = message;
   toast.duration = duration;
   toast.color = color;
+  toast.position = position;
 
   document.body.appendChild(toast);
   return toast.present();
