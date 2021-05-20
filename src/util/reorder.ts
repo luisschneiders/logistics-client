b544ebc06887a19ae40acb211dd45eb5fa61cc2b
Luis Schneiders <schneiders.tech@gmail.com>
2021-05-21 06:56:03 +1000

    add immutable reorder function

---
+++ src/util/reorder.ts
@@ -0,0 +1,20 @@
+export const immutableReorder = (data: any[], from: number, to: number) => {
+  return data.reduce((prev, current, idx, self) => {
+    if (from === to) {
+      prev.push(current);
+    }
+    if (idx === from) {
+      return prev;
+    }
+    if (from < to) {
+      prev.push(current);
+    }
+    if (idx === to) {
+      prev.push(self[from]);
+    }
+    if (from > to) {
+      prev.push(current);
+    }
+    return prev;
+  }, []);
+}
