d93f42cd2cadd07e6d26e44401ba7760da6bbb7c
Luis Schneiders <schneiders.tech@gmail.com>
2021-05-24 06:56:12 +1000

    add React.memo; re-order data array when drag&drop is perform;

--- src/components/list/GroupCollectionDelivery.tsx
+++ src/components/list/GroupCollectionDelivery.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   IonLabel,
   IonList,
@@ -10,6 +10,7 @@ import {
 import LsCollectionDelivery from './CollectionDelivery';
 import { AppColor } from '../../enum/AppColor';
 import { CollectionDelivery } from '../../models/CollectionDelivery';
+import { immutableReorder } from '../../util/reorder';
 
 interface ContainerProps {
   data: any;
@@ -18,38 +19,54 @@ interface ContainerProps {
 
 const LsGroupCollectionDelivery: React.FC<ContainerProps> = ({data, groupBy}) => {
 
+  const [dataOrder, setDataOrder] = useState<any[]>([]);
+
+  useEffect(() => {
+    setDataOrder(data.group);
+  },[
+    data,
+  ]);
+
   const doReorder = (event: any) => {
     // Finish the reorder and position the item in the DOM based on
     // where the gesture ended. This method can also be called directly
     // by the reorder group
     event.detail.complete();
+
+    // The parseInt(event.target.id) is the ID from <IonReorderGroup>
+    const newDataOrder: any[] = immutableReorder(dataOrder[parseInt(event.target.id)], event.detail.from, event.detail.to);
+    const newData: any[] = dataOrder;
+
+    // Replace with new order
+    newData[parseInt(event.target.id)] = newDataOrder;
+
+    setDataOrder(newData);
+
   }
 
   return (
-    <>
-      <IonList lines="full">
-        {data.group.map((groups: any, index: number) => (
-          <IonItemGroup key={`group-${index}`}>
-            <IonItemDivider sticky>
-              <IonLabel>
-                <IonText color={AppColor.PRIMARY}>
-                  <h3 className="ion-text-uppercase">{`${groups[0][groupBy]} run`}</h3>
-                </IonText>
-              </IonLabel>
-            </IonItemDivider>
-            <IonReorderGroup disabled={false} onIonItemReorder={doReorder}>
-              {groups.map((data: CollectionDelivery, itemIndex: number) => (
-                <LsCollectionDelivery
-                  index={index}
-                  data={data}
-                  key={`group-${index}-${itemIndex}`} />
-              ))}
-            </IonReorderGroup>
-          </IonItemGroup>
-        ))}
-      </IonList>
-    </>
+    <IonList lines="full">
+      {dataOrder.map((groups: any, index: number) => (
+        <IonItemGroup key={`group-${index}`}>
+          <IonItemDivider sticky>
+            <IonLabel>
+              <IonText color={AppColor.PRIMARY}>
+                <h3 className="ion-text-uppercase">{`${groups[0][groupBy]} run`}</h3>
+              </IonText>
+            </IonLabel>
+          </IonItemDivider>
+          <IonReorderGroup disabled={false} onIonItemReorder={doReorder} id={index.toString()}>
+            {groups.map((data: CollectionDelivery, itemIndex: number) => (
+              <LsCollectionDelivery
+                index={index}
+                data={data}
+                key={`group-${index}-${itemIndex}`} />
+            ))}
+          </IonReorderGroup>
+        </IonItemGroup>
+      ))}
+    </IonList>
   );
 };
 
-export default LsGroupCollectionDelivery;
+export default React.memo(LsGroupCollectionDelivery);
