3bef7d4bd2a5863804b99683081c74d46654aa24
Luis Schneiders <schneiders.tech@gmail.com>
2021-07-22 07:51:50 +1000

    component Transition now accepts datetime input with formats based on year, month and day

--- src/components/time/Transition.tsx
+++ src/components/time/Transition.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Transition.scss';
 import {
   arrowBackOutline,
   arrowForwardOutline
@@ -7,17 +8,20 @@ import { AppColor } from '../../enum/AppColor';
 import {
   IonButton,
   IonCol,
+  IonDatetime,
   IonGrid,
   IonIcon,
   IonRow
 } from '@ionic/react';
 import { Period } from '../../models/Period';
 import {
-  currentPeriod,
-  dateFormatDDMMYYYY,
+  dateFormatYYYYMMDD,
   decreasePeriod,
+  endPeriod,
   increasePeriod,
+  startPeriod,
 } from '../../util/moment';
+import * as MOMENT  from '../../util/moment';
 
 interface ContainerProps {
   dayOrMonthOrYear: 'day' | 'month' | 'year';
@@ -26,21 +30,89 @@ interface ContainerProps {
 }
 
 const LsTransition: React.FC<ContainerProps> = ({ dayOrMonthOrYear, period, setPeriod }) => {
+  const [deliveryDate, setDeliveryDate] = useState<string>(MOMENT.now.toString());
+  const [datetimeDisplayFormat, setDatetimeDisplayFormat] = useState<string>('');
+
+  useEffect(() => {
+
+    if (dayOrMonthOrYear === 'year') {
+      setDatetimeDisplayFormat('YYYY');
+    } else if (dayOrMonthOrYear === 'month') {
+      setDatetimeDisplayFormat('MMM YYYY');
+    } else {
+      setDatetimeDisplayFormat('MMM, DD YYYY');
+    }
+
+    setDeliveryDate(period.startDate);
+
+  },[
+    period,
+    dayOrMonthOrYear,
+  ]);
+
+  const handleOnChange = async (e: any) => {
+
+    let period: Period = {
+      startDate: '',
+      endDate: ''
+    };
+
+    if (dayOrMonthOrYear === 'year') {
+      period = {
+        startDate: startPeriod(e.detail.value, 'year'),
+        endDate: endPeriod(e.detail.value, 'year'),
+      }
+    } else if (dayOrMonthOrYear === 'month') {
+      period = {
+        startDate: startPeriod(e.detail.value),
+        endDate: endPeriod(e.detail.value),
+      }
+    } else {
+      period = {
+        startDate: dateFormatYYYYMMDD(e.detail.value),
+        endDate: dateFormatYYYYMMDD(e.detail.value),
+      }
+    }
+
+    setDeliveryDate(e.detail.value!);
+    setPeriod(period);
+
+  }
+
+  const setDecreasePeriod = async () => {
+    const newPeriod: Period = decreasePeriod(period, dayOrMonthOrYear)
+    setPeriod(newPeriod);
+  }
+
+  const setIncreasePeriod = async () => {
+    const newPeriod: Period = increasePeriod(period, dayOrMonthOrYear)
+    setPeriod(newPeriod);
+  }
+
   return (
-    <IonGrid>
+    <IonGrid className="transition">
       <IonRow>
         <IonCol className="ion-text-right ion-no-padding">
-          <IonButton color={AppColor.LIGHT} size="small" onClick={() => setPeriod(decreasePeriod(period, dayOrMonthOrYear))}>
+          <IonButton color={AppColor.LIGHT} size="small" onClick={setDecreasePeriod}>
             <IonIcon icon={arrowBackOutline}/>
           </IonButton>
         </IonCol>
         <IonCol className="ion-text-center ion-no-padding">
-          <IonButton color={AppColor.LIGHT} size="small" onClick={() => setPeriod(currentPeriod(dayOrMonthOrYear))}>
-            {`${dateFormatDDMMYYYY(period.startDate)} - ${dateFormatDDMMYYYY(period.endDate)}`}
+          <IonButton
+            color={AppColor.LIGHT}
+            size="small"
+          >
+            <IonDatetime
+              className="transition__datetime"
+              displayFormat={datetimeDisplayFormat}
+              placeholder="Delivery Date"
+              value={deliveryDate}
+              onIonChange={handleOnChange}
+            />
           </IonButton>
         </IonCol>
         <IonCol className="ion-text-left ion-no-padding">
-          <IonButton color={AppColor.LIGHT} size="small" onClick={() => setPeriod(increasePeriod(period, dayOrMonthOrYear))}>
+          <IonButton color={AppColor.LIGHT} size="small" onClick={setIncreasePeriod}>
             <IonIcon icon={arrowForwardOutline}/>
           </IonButton>
         </IonCol>
